b6a652223717e182bf0bcc485cf8f37235266980
unknown <rijoanul.shanto@gmail.com>
2020-05-14 10:33:22 -0700

    admin page customized

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'popularity', 'is_availabe']
+
+class ChoiceAdmin(admin.ModelAdmin):
+    # fieldsets = [
+    #     ('question',     {'fields': ['choice_text', 'votes']})
+    # ]
+    list_display = ['question', 'choice_text', 'votes']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', auto_now=True)
     is_availabe = models.BooleanField(default=True)
     popularity = models.IntegerField(default=0)
 
